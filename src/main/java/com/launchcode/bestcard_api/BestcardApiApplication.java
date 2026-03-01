af284a1ae4a3d0b7a58ac83841d26cd399e3952f
Hursha Parveen <hurshaparveen@gmail.com>
2026-03-01 14:40:58 -0600

    Initial commit.

---
+++ src/main/java/com/launchcode/bestcard_api/BestcardApiApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.bestcard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestcardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestcardApiApplication.class, args);
+	}
+
+}
